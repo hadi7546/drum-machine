c15550f0cf693875a7a39420a2ad52be44edafe5
hadi <hazard7546@gmail.com>
2025-09-22 12:50:22 +0330

    fix(ci): Correct build script path

--- scripts/build-macos.sh
+++ scripts/build-macos.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# Go to the project root
-cd ..
-
 # Set up the build directory
 meson setup builddir
 
--- scripts/build-windows.sh
+++ scripts/build-windows.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# Go to the project root
-cd ..
-
 # Set up the build directory for Windows cross-compilation
 meson setup builddir --cross-file=scripts/windows-cross-file.txt
 
